3e7fb654fc5a0f2a0f775773c6c471e1945e8a18
viktor d <10xl.pro@mail.ru>
2023-12-23 13:51:07 +0300

    Добавлены классы для работы с базой данных

--- Vladlen/AppDbContext.cs
+++ Vladlen/AppDbContext.cs
@@ -1,6 +1,26 @@
-﻿namespace Vladlen
+﻿using Microsoft.EntityFrameworkCore;
+using Vladlen.Entities;
+
+namespace Vladlen
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        // Таблица клиентов
+        public DbSet<Client> Clients { get; set; }
+        // Таблица продуктов
+        public DbSet<Product> Products { get; set; }
+        // Таблица документов
+        public DbSet<Document> Documents { get; set; }
+
+        public AppDbContext()
+        {
+            // Существует ли база данных
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Строка подключения к базе данных
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=DbProductShop;Trusted_Connection=True;");
+        }
     }
 }
---
+++ Vladlen/LocalDataBase.cs
@@ -0,0 +1,14 @@
+﻿using Vladlen.Entities;
+
+namespace Vladlen
+{
+    public class LocalDataBase
+    {
+        // Локальный список клиентов
+        public List<Client> Clients {  get; set; }
+        // Локальный список продуктов
+        public List<Product> Products { get; set; }
+        // Локальный список документов
+        public List<Document> Documents { get; set; }
+    }
+}
