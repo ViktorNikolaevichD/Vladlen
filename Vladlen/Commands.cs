8633be7a7b8a67b4ec9b5bba2f7dc3d4011b5d29
viktor d <10xl.pro@mail.ru>
2023-12-24 13:38:39 +0300

    Добавлены функции и команды для их вызова в MPI

--- Vladlen/Commands.cs
+++ Vladlen/Commands.cs
@@ -5,6 +5,102 @@ namespace Vladlen
 {
     public class Commands
     {
+        // Обновить базу данных на сервере
+        public static void UpdateDataBase(LocalDataBase localDb)
+        {
+            using (var db = new AppDbContext())
+            {
+                // Обновить таблицу клиентов
+                db.Clients.UpdateRange(localDb.Clients);
+                // Обновить таблицу продуктов
+                db.Products.UpdateRange(localDb.Products);
+                // Обновить таблицу документов
+                db.Documents.UpdateRange(localDb.Documents);
+                
+                // Зафиксировать измения в БД
+                db.SaveChanges();
+            }
+        }
+        // Изменить стоимость продукта
+        public static void EditCost(LocalDataBase localDb, int idProduct, decimal newCost)
+        {
+            // Продукт в таблице Products
+            var product = localDb.Products.Where(p => p.ProductId == idProduct).FirstOrDefault();
+            // Если нашлась строчка с продуктом
+            if (product != null)
+            {
+                // Установить новую цену
+                product.Cost = newCost;
+            }
+            // Продукт в таблице Documents
+            var productFromDocuments = localDb.Documents.Where(p => p.ProductId == idProduct).Select(u => u.Product).FirstOrDefault();
+            // Если нашлась строчка с продуктом в таблице Documents
+            if (productFromDocuments != null)
+            {
+                // установить новую цену
+                productFromDocuments.Cost = newCost;
+            }
+        }
+        // Удалить документ о покупке
+        public static Document? DeleteDocument(int documentId)
+        {
+            using (var db = new AppDbContext())
+            {
+                // Найти документа в таблице
+                var document = db.Documents
+                    .Include(u => u.Client)
+                    .Include(u => u.Product)
+                    .Where(p => p.DocumentId == documentId)
+                    .FirstOrDefault();
+                // Если документа не нашлось
+                if (document == null) 
+                {
+                    return null;
+                }
+                // Удалить документ из базы данных на сервере
+                db.Documents.Remove(document);
+                // Зафиксировать изменения
+                db.SaveChanges();
+                return document;
+            } 
+        }
+        // Продать пользователю по Id товар по Id с определенным количеством
+        public static void SellProduct(int clientId, int productId, int countProducts)
+        {
+            using (var db = new AppDbContext())
+            {
+                // Получить клиента из локальной базы данных
+                var client = db.Clients.Where(p => p.ClientId == clientId).FirstOrDefault();
+                // Если клиента не нашлось, то выйти из функции
+                if (client == null)
+                {
+                    return;
+                }
+                // Получить продукт из локальной базы данных
+                var product = db.Products.Where(p => p.ProductId == productId).FirstOrDefault();
+                // Если продкта не нашлось, то выйти из функции
+                if (product == null)
+                {
+                    return;
+                }
+                // Добавить в базу данных документ о продаже продукции
+                db.Documents.Add(new Document { ClientId = clientId, ProductId = productId, Count = countProducts });
+                // Зафиксировать изменения в базе данных
+                db.SaveChanges();
+            }
+        }
+        // Вывести документы о покупках клиента
+        public static List<Document> GetDocuments(LocalDataBase localDb, int clientId)
+        {
+            // Найти в локальной базе данных заказы клиента по Id. Вернуть все в виде списка
+            return localDb.Documents.Where(u => u.ClientId == clientId).ToList();
+        }
+        // Найти клиента по Id
+        public static Client? GetClient(LocalDataBase localDb, int clientId)
+        {
+            // Найти в локальной базе данных клиента по Id. Если не нашлось, то вернуть null
+            return localDb.Clients.Where(p => p.ClientId == clientId).FirstOrDefault();
+        }
         // Загрузить базу данных
         public static LocalDataBase DownloadDb(int rank, int size)
         {
@@ -51,7 +147,8 @@ namespace Vladlen
                 List<Client> clients = new List<Client> { };
                 // Локальный список продуктов
                 List<Product> products = new List<Product> { };
-                
+                // Объект Random
+                Random rand = new Random();
                 // Цикл для генерации случайных пользователей и продуктов
                 for (int i = 1; i < count + 1; i++)
                 {
@@ -69,11 +166,14 @@ namespace Vladlen
                     db.Clients.Add(client);
                     // Добавить в локальный список клиента
                     clients.Add(client);
+
                     // Случайный продукт
                     Product product = new Product
                     {
                         // Для 0 процесса -> Product{0 + 1 + 0 * count}
-                        ProductName = $"Product{maxId + i}"
+                        ProductName = $"Product{maxId + i}",
+                        // Случайная стоимость товара
+                        Cost = rand.Next(25, 256)
                     };
                     // Добавить в базу данных случайный продукт
                     db.Products.Add(product);
@@ -82,8 +182,6 @@ namespace Vladlen
                 }
                 // Зафиксировать изменения в базе данных
                 db.SaveChanges();
-                // Объект Random
-                Random rand = new Random();
                 // Цикл для генерации случайных документов о покупке
                 for (int i = 0;  i < count; i++)
                 {
--- Vladlen/Program.cs
+++ Vladlen/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using Vladlen.Entities;
+using System.Diagnostics;
+using System.Text.Json;
 
 namespace Vladlen
 {
@@ -8,81 +10,260 @@ namespace Vladlen
         {
             MPI.Environment.Run(ref args, comm =>
             {
-            // Экземпляр локальной базы данных
-            LocalDataBase localDb = new LocalDataBase();
-            // Предзагрузка базы данных
-            if (comm.Rank == 0)
-            {
-                // 0 процесс загружает локальную базу данных
-                localDb = Commands.DownloadDb(comm.Rank, comm.Size);
-            }
-            // Ожидание всем процессами завершения загрузки базы данных 0 процессом
-            comm.Barrier();
-            if (comm.Rank != 0)
-            {
-                // все !0 процессы загружают локальную базу данных
-                localDb = Commands.DownloadDb(comm.Rank, comm.Size);
-            }
-            // Объект для измерения времени
-            Stopwatch stopWatch = new Stopwatch();
-            // Переменная для команд пользователя
-            string? command = null;
-            // Бесконечное ожидание команд и их выполнение, пока не quit
-            while (command != "quit")
-            {
-                // Выполняет 0 процесс
+                // Экземпляр локальной базы данных
+                LocalDataBase localDb = new LocalDataBase();
+                // Предзагрузка базы данных
                 if (comm.Rank == 0)
                 {
-                    // Список команд в консоль
-                    Console.Write("Введите команду: ");
-                    // Команда пользователя
-                    command = Console.ReadLine();
+                    // 0 процесс загружает локальную базу данных
+                    localDb = Commands.DownloadDb(comm.Rank, comm.Size);
                 }
-                // Рассылка команды из 0 процесса по остальным процессам
-                comm.Broadcast(ref command, 0);
-                switch (command)
+                // Ожидание всем процессами завершения загрузки базы данных 0 процессом
+                comm.Barrier();
+                if (comm.Rank != 0)
                 {
-                    // Генерация данных в БД
-                    case "generate":
-                        // Количество строк
-                        int count = 0;
-                        if (comm.Rank == 0)
-                        {
-                            Console.Write("Какое количество строк сгенерировать: ");
-                            count = Convert.ToInt32(Console.ReadLine());
+                    // все !0 процессы загружают локальную базу данных
+                    localDb = Commands.DownloadDb(comm.Rank, comm.Size);
+                }
+                // Объект для измерения времени
+                Stopwatch stopWatch = new Stopwatch();
+                // Переменная для команд пользователя
+                string? command = null;
+                // Бесконечное ожидание команд и их выполнение, пока не quit
+                while (command != "quit")
+                {
+                    // Выполняет 0 процесс
+                    if (comm.Rank == 0)
+                    {
+                        // Список команд в консоль
+                        Console.Write(
+                            "Введите команду" +
+                            "\nupdate - обновить базу данных;" +
+                            "\ncost - изменить стоимость продукта;" +
+                            "\ndelete - удалить документ (отмена сделки);" +
+                            "\ndocuments - вывести документы о покупках;" +
+                            "\nsell - продать продукт;" +
+                            "\nclient - вывести информацию о клиенте;" +
+                            "\ngenerate - сгенерировать данные;" +
+                            "\nquit - выйти: "
+                            );
+                        // Команда пользователя
+                        command = Console.ReadLine();
+                    }
+                    // Рассылка команды из 0 процесса по остальным процессам
+                    comm.Broadcast(ref command, 0);
+                    switch (command)
+                    {
+                        // Обновление базы данных на сервере
+                        case "update":
+                            stopWatch.Restart();
+                            if (comm.Rank == 0)
+                                Console.WriteLine("Обновляем базу");
+                            // Обновление базы данных
+                            Commands.UpdateDataBase(localDb);
+                            if (comm.Rank == 0)
+                                Console.WriteLine("База данных обновлена");
+                            stopWatch.Stop();
+                            break;
+                        // Обновление стоимости товара
+                        case "cost":
+                            // Айди продукта
+                            int productId = 0;
+                            // Новая стоимость
+                            int newCost = 0;
+                            if (comm.Rank == 0)
+                            {
+                                Console.Write("Введите Id продукта, чтобы изменить стоимость: ");
+                                productId = Convert.ToInt32(Console.ReadLine());
+                                Console.Write("Введите новую стоимость продукта: ");
+                                newCost = Convert.ToInt32(Console.ReadLine());
+                                Console.WriteLine("Обновляем стоимость");
+                            }
+                            // Разослать по процессам айди продукта
+                            comm.Broadcast(ref productId, 0);
+                            // Разослать по процессам новую стоимость продукта
+                            comm.Broadcast(ref newCost, 0);
+                            // Изменить стоимость продукта
+                            Commands.EditCost(localDb, productId, newCost);
+                            if (comm.Rank == 0)
+                                Console.WriteLine("Стоимость обновлена");
+                            break;
+                        // Удалить документ о продаже (отмена сделки)
+                        case "delete":
+                            int documentId = 0;
+                            if (comm.Rank == 0)
+                            {
+                                Console.Write("Введите Id документа, который хотите удалить: ");
+                                documentId = Convert.ToInt32(Console.ReadLine());
+                            }
+                            stopWatch.Restart();
+                            if (comm.Rank == 0)
+                            {
+                                // Удалить документ о покупке
+                                Document? document = Commands.DeleteDocument(documentId);
+                                if (document == null)
+                                {
+                                    Console.WriteLine("Такого документа не нашлось");
+                                    stopWatch.Stop();
+                                    break;
+                                }
+                                Console.WriteLine($"Удален документ:" +
+                                    $"\nId: {document.DocumentId} Product: {document.Product.ProductName} Cost: {document.Product.Cost} Count: {document.Count}" +
+                                    $"\nClientName: {document.Client.FullName}"); 
+                            }
+                            // Все процессы дожидаются удаления данных
+                            comm.Barrier();
+                            // Обновление локальной базы данных после обновления
+                            localDb = Commands.DownloadDb(comm.Rank, comm.Size);
+                            stopWatch.Stop();
+                            break;
+                        // Продать продукт
+                        case "sell":
+                            // Айди клиента
+                            int clientId = 0;
+                            // Айди продукта
+                            productId = 0;
+                            // Количество товара
+                            int countProduct = 0;
+                            if (comm.Rank == 0)
+                            {
+                                Console.Write("Введите Id клиента для вывода документов: ");
+                                clientId = Convert.ToInt32(Console.ReadLine());
+                                Console.Write("Введите Id продукта для оформления документов: ");
+                                productId = Convert.ToInt32(Console.ReadLine());
+                                Console.Write("Введите количество продуктов: ");
+                                countProduct = Convert.ToInt32(Console.ReadLine());
 
-                            // Сообщить о начале генерации
-                            Console.WriteLine("Начинаем генерацию");
-                        }
-                        // Начать замер времени всеми процессами
-                        stopWatch.Restart();
-                        if (comm.Rank == 0)
-                            // Генерация данных
-                            Commands.GenerateData(count);
-                        // Дождаться генерации базы данных 0 процессом
-                        comm.Barrier();
-                        // Обновление локальной базы данных всеми процессами
-                        localDb = Commands.DownloadDb(comm.Rank, comm.Size);
-                        // Завершить замер времени
-                        stopWatch.Stop();
-                        if (comm.Rank == 0)
-                            Console.WriteLine("Данные сгенерированы");
-                        break;
+                                // Продать продукт пользователю
+                                Commands.SellProduct(clientId, productId, countProduct);
+                            }
+                            // Все процессы дожидаются добавления документов
+                            comm.Barrier();
+                            // Обновить локальную базу данных
+                            localDb = Commands.DownloadDb(comm.Rank, comm.Size);
+                            stopWatch.Stop();
+                            break;
+                        // Вывести документы о покупках
+                        case "documents":
+                            // Айди клиента
+                            clientId = 0;
+                            if (comm.Rank == 0)
+                            {
+                                Console.Write("Введите Id клиента для вывода документов: ");
+                                clientId = Convert.ToInt32(Console.ReadLine());
+                            }
+                            // Разослать всем процессам Id клиента
+                            comm.Broadcast(ref clientId, 0);
+                            stopWatch.Restart();
+                            if (comm.Rank == 0)
+                            {
+                                // Собрать в переменную данные со всех процессов
+                                var gatherData = comm.Gather(JsonSerializer.Serialize(Commands.GetDocuments(localDb, clientId)), 0);
+                                // Десериализовать данные
+                                List<Document> documentsInfo = gatherData
+                                .Select(x => JsonSerializer.Deserialize<List<Document>>(x)!)
+                                .Where(x => x != null)
+                                .Aggregate((a, b) => a.Concat(b).ToList());
+                                
+                                // Проверка, чтобы результат был не пустой
+                                if (!documentsInfo.Any() || documentsInfo.Count() < 1)
+                                {
+                                    Console.WriteLine("Документов не найдено");
+                                    stopWatch.Stop();
+                                    break;
+                                }
+                                // Вывод данных о документах
+                                foreach(var document in documentsInfo)
+                                {
+                                    Console.WriteLine($"{document.DocumentId} {document.Product.ProductName} {document.Count}");
+                                }
+                            }
+                            else
+                            {
+                                // Собрать в 0 процессе данные о запрошенных документах
+                                comm.Gather(JsonSerializer.Serialize(Commands.GetDocuments(localDb, clientId)), 0);
+                            }
+                            stopWatch.Stop();
+                            break;
+                        // Вывести данные о клиенте по Id
+                        case "client":
+                            // Айди клиента
+                            clientId = 0;
+                            if (comm.Rank == 0)
+                            {
+                                Console.Write("Введите Id клиента для вывода информации: ");
+                                clientId = Convert.ToInt32(Console.ReadLine());
+                            }
+                            // Разослать всем процессам Id клиента
+                            comm.Broadcast(ref clientId, 0);
+                            stopWatch.Restart();
+                            if (comm.Rank == 0)
+                            {
+                                // Собрать в переменную данные со всех процессов
+                                var gatherData = comm.Gather(JsonSerializer.Serialize(Commands.GetClient(localDb, clientId)), 0);
+                                // Отсеить null значения и получить строчку клиента или null значение(если пользователя нет нигде)
+                                Client? clientInfo = gatherData
+                                .Select(x => JsonSerializer.Deserialize<Client>(x))
+                                .Where(x => x != null)
+                                .FirstOrDefault();
+
+                                if (clientInfo == null)
+                                {
+                                    Console.WriteLine("Клиента не найдено");
+                                    stopWatch.Stop();
+                                    break;
+                                }
+                                // Вывод данных о пользователе
+                                Console.WriteLine($"Id: {clientInfo.ClientId} FullName: {clientInfo.FullName} PhoneNumber: {clientInfo.PhoneNumber}");
+                            }
+                            else
+                            {
+                                // Собрать в 0 процессе данные о запрошенном клиенте
+                                comm.Gather(JsonSerializer.Serialize(Commands.GetClient(localDb, clientId)), 0);
+                            }
+                            stopWatch.Stop();
+                            break;
+                        // Генерация данных в БД
+                        case "generate":
+                            // Количество строк
+                            int count = 0;
+                            if (comm.Rank == 0)
+                            {
+                                Console.Write("Какое количество строк сгенерировать: ");
+                                count = Convert.ToInt32(Console.ReadLine());
+
+                                // Сообщить о начале генерации
+                                Console.WriteLine("Начинаем генерацию");
+                            }
+                            // Начать замер времени всеми процессами
+                            stopWatch.Restart();
+                            if (comm.Rank == 0)
+                                // Генерация данных
+                                Commands.GenerateData(count);
+                            // Дождаться генерации базы данных 0 процессом
+                            comm.Barrier();
+                            // Обновление локальной базы данных всеми процессами
+                            localDb = Commands.DownloadDb(comm.Rank, comm.Size);
+                            // Завершить замер времени
+                            stopWatch.Stop();
+                            if (comm.Rank == 0)
+                                Console.WriteLine("Данные сгенерированы");
+                            break;
                         // Любая неизвестная команда
                         default:
                             if (comm.Rank == 0 && command != "quit")
                                 Console.WriteLine("Неизвестная команда");
                             break;
                     }
-                    // Собрать все процессы в одном месте, чтобы одновременно вывести
-                    comm.Barrier();
-                    // Вывод времени работы в консоль каждым процессом
-                    TimeSpan ts = stopWatch.Elapsed;
-                    string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-                    ts.Hours, ts.Minutes, ts.Seconds,
-                    ts.Milliseconds / 10);
-                    Console.WriteLine($"RunTime {comm.Rank} " + elapsedTime);
-                    // Подождать окончания вывода замеров всеми процессами
+                    if (comm.Rank == 0)
+                    {
+                        // Вывод времени работы в консоль 0 процессом
+                        TimeSpan ts = stopWatch.Elapsed;
+                        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                        ts.Hours, ts.Minutes, ts.Seconds,
+                        ts.Milliseconds / 10);
+                        Console.WriteLine($"RunTime {comm.Rank} " + elapsedTime);
+                    }
                     comm.Barrier();
                 }
             });
